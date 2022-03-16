da1deadbc50e95a27afdd0aa75663cec95882a8a
SreejaPaulG41 <sreejap@geekyants.com>
2022-03-16 19:31:37 +0530

    Leaderboard API Done

--- router/leaderBoard.js
+++ router/leaderBoard.js
@@ -5,9 +5,9 @@ const { Op } = require('sequelize');
 const { Answers, Genres, Questions, Users } = require("../models");
 const auth = require('../middleware/auth');
 
+//Genre Specific Leaderboard
 router.get("/leaderBoard-genreSpecific", auth, async (req, res) => {
     try {
-        const userId = parseInt(req.user);
         const { genreId } = req.body;
         //Get All The Answer Result
         const answers = await Answers.findAll({
@@ -15,30 +15,19 @@ router.get("/leaderBoard-genreSpecific", auth, async (req, res) => {
                 genreId
             }
         })
-        //Get The User Name
+        //Get The User Given The Quiz
         const userPresent = answers?.map((item, index) => {
             return item.userId
         })
-        const userInfo = await Users.findAll({
-            attributes: ["firstName", "lastName"],
-            where: {
-                id: {
-                    [Op.or]: userPresent
-                }
-            }
-        })
-        //Get Genre Specific Question
-        const questions = await Questions.findAll({
+        //Get Genre Specific Question Id and Marks
+        const questionMarks = await Questions.findAll({
+            attributes: ["questionId", "questionMark"],
             where: {
                 genreId
-            }
+            },
+            raw: true
         })
-        const questionMarks = questions.map((item, index) => {
-            return {
-                questionId: item.questionId,
-                questionMark: item.questionMark
-            }
-        })
+        //Total Quiz Marks
         const totalMarksOfQuiz = questionMarks.reduce((acc, item) => {
             acc = acc + item.questionMark;
             return acc;
@@ -55,7 +44,6 @@ router.get("/leaderBoard-genreSpecific", auth, async (req, res) => {
         })
 
         //Calculate Marks
-
         let marksOfUser = [];
         for (let i = 0; i < answer.length; i++) {
             let userId = answer[i].userId;
@@ -78,13 +66,111 @@ router.get("/leaderBoard-genreSpecific", auth, async (req, res) => {
                 acc = acc + item;
                 return acc;
             }, 0);
-            marksOfUser.push({ genreId, userId, totalMarksGot, totalMarksOfQuiz })
+            marksOfUser.push({ userId, totalMarksGot })
         }
-        res.json(marksOfUser)
+        //Send Data To Frontend
+        const userDetailsToSend = await Promise.all(marksOfUser.map(async (item, index) => {
+            const userInfo = await Users.findOne({
+                attributes: ["firstName", "lastName"],
+                where: {
+                    id: item.userId
+                }
+            })
+            const userName = userInfo.firstName + " " + userInfo.lastName;
+            return {
+                ...item,
+                genreId,
+                userName,
+                totalMarksOfQuiz
+            }
+        }))
+
+        res.json(userDetailsToSend)
     } catch (error) {
         console.log(error);
         res.status(403).send("Not Authorized User!");
     }
 })
 
+//User Specific Leaderboard
+router.get("/leaderBoard-userSpecific", auth, async (req, res) => {
+    try {
+        const userId = parseInt(req.user);
+        //Get User Specific Answeres
+        const answersInfoOfUser = await Answers.findAll({
+            attributes: ["userId", "genreId", "givenAnswerDetails", "createdAt"],
+            where: {
+                userId
+            }
+        })
+        //Genre Specific Question Details
+        const questionMarksDetails = await Promise.all(answersInfoOfUser.map(async (item, index) => {
+            const questions = await Questions.findAll({
+                attributes: ["questionId", "questionMark"],
+                where: {
+                    genreId: item.genreId
+                },
+                raw: true
+            })
+            const totalQuestionMarks = questions.reduce((acc, item) => {
+                acc = acc + item.questionMark;
+                return acc;
+            }, 0)
+
+            return { questions: questions, totalQuestionMarks, genreId: item.genreId };
+        }))
+
+        //Calculate Marks 
+        const marksForEachGenre = [];
+        for (let i = 0; i < answersInfoOfUser.length; i++) {
+            const genre = answersInfoOfUser[i].genreId;
+            const answers = answersInfoOfUser[i].givenAnswerDetails;
+            const createdAt = answersInfoOfUser[i].createdAt;
+            const marks = answers.map((item, index) => {
+                let marksToStore = 0;
+                if (item.answerGiven) {
+                    const questionDetailsArr = questionMarksDetails.filter((item, index) => {
+                        return item.genreId == genre;
+                    })
+                    const fullMarks = questionDetailsArr[0].questions.find((data, index) => {
+                        return data.questionId == item.questionId;
+                    })
+                    if (item.rightNess) {
+                        marksToStore = marksToStore + fullMarks.questionMark;
+                    } else {
+                        marksToStore = marksToStore - (fullMarks.questionMark * 0.50);
+                    }
+                }
+                return marksToStore;
+            })
+            const getTotalMarks = questionMarksDetails.find((item, index) => {
+                return item.genreId == genre;
+            })
+            marksForEachGenre.push({ marks, genre, totalMarks: getTotalMarks.totalQuestionMarks, createdAt })
+        }
+        //Add Total Marks Got
+        const addedTotalGotMarks = marksForEachGenre.map((item, index) => {
+            const totalMarksGot = item.marks.reduce((acc, item) => {
+                acc = acc + item;
+                return acc;
+            })
+            return { genre: item.genre, totalMarks: item.totalMarks, createdAt: item.createdAt, totalMarksGot }
+        })
+        //Send Data To Frontend, After Adding All
+        const dataToSend = await Promise.all(addedTotalGotMarks.map(async (item, index) => {
+            const genreName = await Genres.findOne({
+                attributes: ["genreName"],
+                where: {
+                    genreId: item.genre
+                }
+            })
+            return { totalMarks: item.totalMarks, totalMarksGot: item.totalMarksGot, createdAt: item.createdAt, genreName: genreName.genreName, userId }
+        }))
+
+        res.json(dataToSend)
+    } catch (error) {
+        console.log(error);
+        res.status(403).send("Not Authorized User!");
+    }
+})
 module.exports = router;
