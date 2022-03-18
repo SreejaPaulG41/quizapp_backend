1c808a415519fb90edebf98f065b828cdc3bd9a9
SreejaPaulG41 <sreejap@geekyants.com>
2022-03-18 19:35:47 +0530

    All Ledaerboard API Changed

--- router/leaderBoard.js
+++ router/leaderBoard.js
@@ -6,24 +6,26 @@ const { Answers, Genres, Questions, Users, Leaderboard } = require("../models");
 const auth = require('../middleware/auth');
 
 //Genre Specific Leaderboard
-router.get("/leaderBoard-genreSpecific", auth, async (req, res) => {
+router.get("/leaderBoard", auth, async (req, res) => {
     try {
-        const { genreId } = req.body;
-        //Get Users Who Gave Quiz For Specific Genre
-        const userInfos = await Leaderboard.findAll({
-            where: {
-                genreId
-            }
-        })
-        //Send Data To Frontend
-        const dataToSend = await Promise.all(userInfos.map( async(item, index)=>{
+        // const { genreId } = req.query;
+        // //Get Users Who Gave Quiz For Specific Genre
+        // const userInfos = await Leaderboard.findAll({
+        //     where: {
+        //         genreId
+        //     }
+        // })
+        const leaderBoardData = await Leaderboard.findAll()
+        // //Send Data To Frontend
+        const dataToSend = await Promise.all(leaderBoardData.map( async(item, index)=>{
             const userName = await Users.findOne({
                 attributes: ["firstName", "lastName"],
                 where: {
                     id: item.userId
                 }
             })
-            return {userFullName: userName.firstName + " " + userName.lastName, fullMarks: item.fullMarks, userScore: item.userScore, quizGivenTime: item.quizGivenTime.toString()}
+            const date = item.quizGivenTime?.getDate() + " / " + item.quizGivenTime?.getMonth() + " / " + item.quizGivenTime?.getFullYear();
+            return {userFullName: userName.firstName + " " + userName.lastName, fullMarks: item.fullMarks, userScore: item.userScore, quizGivenTime: date}
         }))
         res.json(dataToSend)
     } catch (error) {
@@ -49,7 +51,8 @@ router.get("/leaderBoard-userSpecific", auth, async (req, res) => {
                     genreId: item.genreId
                 }
             })
-            return { genreName: genreNameRes.genreName, fullMarks: item.fullMarks, userScore: item.userScore, quizGivenTime: item.quizGivenTime.toString()}
+            const date = item.quizGivenTime?.getDate() + " / " + item.quizGivenTime?.getMonth() + " / " + item.quizGivenTime?.getFullYear();
+            return { genreName: genreNameRes.genreName, genreId: item.genreId, fullMarks: item.fullMarks, userScore: item.userScore, quizGivenTime: date}
         }))
         res.json(dataToSend)
     } catch (error) {
